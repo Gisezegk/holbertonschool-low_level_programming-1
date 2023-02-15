739c63c6cb5d6763e3d35ff75b2a329d78dab315
root <lautarorodriguezcoitinho@gmail.com>
2023-02-15 09:24:21 -0800

    crack

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(void)
 {
-char gise[] = "\"Programming is like building a multilingual puzzle";   
-puts(gise); 
+char gise[] = "\"Programming is like building a multilingual puzzle";
+
+puts(gise);
+
 return (0);
 }
