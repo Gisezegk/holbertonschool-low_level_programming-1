2d28dc9106c4d5d0590513a03bd76cddc9eb8201
root <lautarorodriguezcoitinho@gmail.com>
2023-02-15 09:21:04 -0800

    gise

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    char gise[] = "Programming is like building a multilingual puzzle";   
+    char gise[] = "\"Programming is like building a multilingual puzzle";   
     puts(gise); 
     
     return 0;
