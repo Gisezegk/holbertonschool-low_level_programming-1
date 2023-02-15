fe0fc363d2cdc04e400612f27c8cdfc6a8a2bc8b
root <lautarorodriguezcoitinho@gmail.com>
2023-02-15 13:43:40 -0800

    k

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - number
- * Return: always 0
- */
+* main - number
+* Return: always 0
+*/
 
 int main(void)
 {
@@ -19,7 +19,7 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-	    printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
