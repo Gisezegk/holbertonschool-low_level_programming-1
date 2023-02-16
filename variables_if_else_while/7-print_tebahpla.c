4d619a01450aef84824e613cc48c1d5fea445f9d
root <lautarorodriguezcoitinho@gmail.com>
2023-02-16 06:05:34 -0800

    task0007

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+* main - abc
+* Return: always 0
+*/
+
+int main(void)
+{
+	char abc = 'z';
+
+	for (abc = 'z'; abc >= 'a'; abc--)
+	putchar(abc);
+putchar('\n');
+	return (0);
+}
