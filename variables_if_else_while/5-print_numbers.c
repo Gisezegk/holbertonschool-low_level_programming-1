382ccfaf1636e6b688b133489be548fe4d5665eb
root <lautarorodriguezcoitinho@gmail.com>
2023-02-16 05:34:49 -0800

    task0005

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+* main - 123
+* Return: always 0
+*/
+
+int main(void)
+{
+	char abc = '0';
+
+	for (abc = '0'; abc <= '9'; abc++)
+	putchar(abc);
+putchar('\n');
+	return (0);
+}
