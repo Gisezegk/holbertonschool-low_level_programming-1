50e0101ddb1a21e3491bb33bdcf82828083c87c3
root <lautarorodriguezcoitinho@gmail.com>
2023-02-16 12:06:44 -0800

    k

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,13 @@
 #include"main.h"
 #include"1-alphabet.c"
-
 /**
 * print_alphabet_x10 - prints the alphabet.
 * Return: 0
 */
-
 void print_alphabet_x10(void)
-
 {
 	int iterator = 0;
 
-for ( ; iterator >= 9 ; iterator++)
-	  print_alphabet();
+	for ( ; iterator <= 9 ; iterator++)
+		print_alphabet();
 }
