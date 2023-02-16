edc26e774f16e0a73da14f6190a0f85bca9ecdae
root <lautarorodriguezcoitinho@gmail.com>
2023-02-16 08:55:10 -0800

    taskfinal

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+* main - 123
+* Return: always 0
+*/
+
+
+int main(void)
+{
+	int abc = 0;
+char com = ',';
+char spc = ' ';
+        for (abc = 0; abc <= 9; abc++) 
+	{
+        putchar(abc + '0');
+	if (abc != 9) 
+	{
+	putchar(com);
+	putchar(spc);	
+	}
+	}
+	putchar('\n');
+        return (0);
+}
