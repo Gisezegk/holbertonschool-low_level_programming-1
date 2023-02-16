23a86ea78581e903cece0bf693b09124ca1b369b
root <lautarorodriguezcoitinho@gmail.com>
2023-02-16 09:00:53 -0800

    taskremade

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,15 +11,15 @@ int main(void)
 	int abc = 0;
 char com = ',';
 char spc = ' ';
-        for (abc = 0; abc <= 9; abc++) 
+	for (abc = 0; abc <= 9; abc++)
 	{
-        putchar(abc + '0');
-	if (abc != 9) 
+	putchar(abc + '0');
+	if (abc != 9)
 	{
 	putchar(com);
-	putchar(spc);	
+	putchar(spc);
 	}
 	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
