342c5d3cb4e80a4cbb611d87a15aec9342b9e54c
root <lautarorodriguezcoitinho@gmail.com>
2023-02-16 10:45:08 -0800

    task

---
+++ functions_nested_loops/1_alphabet.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+* main - 12
+* Return: 0
+*/
+
+int main(void)
+{
+char abc = 'a';
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	_putchar(abc);
+
+
+
+	_putchar('\n');
+return (0);
+}
