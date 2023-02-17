20ebeee702dcb38e6b0f8b387a44a1062dadf7d7
root <lautarorodriguezcoitinho@gmail.com>
2023-02-17 05:04:48 -0800

    task

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+* _islower - is lower or no.
+* @c: variable for ch
+* Return: 0
+*/
+
+int _islower(int c)
+{
+
+if (c >= 'a')
+{
+	return (1);
+}
+
+else if (c <= 'z')
+{
+	return (1);
+}
+
+else
+{
+	return (0);
+
+}
+
+}
