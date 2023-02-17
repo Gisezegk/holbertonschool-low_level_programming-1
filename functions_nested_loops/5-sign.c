bd8945d3ce3efc36d01dd26d6d70c4bb3c369ab5
root <lautarorodriguezcoitinho@gmail.com>
2023-02-17 05:43:29 -0800

    xd

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+* print_sign - is positive or no.
+* @n: variable for number
+* Return: 0
+*/
+
+int print_sign(int n)
+{
+
+	int ch = '+';
+	int chm = '-';
+
+	if (n >= 1)
+	{
+		_putchar(ch);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(chm);
+	return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
