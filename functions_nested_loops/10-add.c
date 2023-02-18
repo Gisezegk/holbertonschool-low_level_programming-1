cd4e856e9df18ae9c977d311facde1491282d667
root <lautarorodriguezcoitinho@gmail.com>
2023-02-17 17:27:36 -0800

    ready

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+* add - is positive or no.
+* @palta: no cacauate
+* @chacho: nacho con ch
+* Return: 0
+*/
+
+
+int add(int palta, int chacho)
+{
+	return (palta + chacho);
+}
