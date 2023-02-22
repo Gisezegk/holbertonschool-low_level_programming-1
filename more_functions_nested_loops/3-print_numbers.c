9dcecc81fa81f6c3b0c10512e3cd069eab89e9d1
root <lautarorodriguezcoitinho@gmail.com>
2023-02-22 12:40:44 -0800

    t

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+* print_numbers - prints the alphabet.
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+char abc = 'a';
+
+	for (abc = '0'; abc <= '9'; abc++)
+	_putchar(abc);
+
+
+
+	_putchar('\n');
+}
