b7682b4e177f3d723781ecd6929458f696a1c296
root <lautarorodriguezcoitinho@gmail.com>
2023-02-22 11:48:01 -0800

    uploadtask

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _isupper - print upper
+ */
+
+int _isupper(int c)
+       {
+if (c >= 'A' && c <= 'B')
+{
+        return (1);
+}
+else
+{
+        return (0);
+
+}
+}i	
