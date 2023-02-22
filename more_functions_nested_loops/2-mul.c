526dc564007db46ebde642231f49567d4380065d
root <lautarorodriguezcoitinho@gmail.com>
2023-02-22 12:30:34 -0800

    fixbetty

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - print upper
+ *
+ * Return: 1 if Upper
+ *
+ * @a: character v
+ *
+ * @b: character numb
+ *
+ * @c: result
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
