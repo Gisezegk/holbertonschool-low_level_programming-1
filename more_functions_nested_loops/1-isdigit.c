478f0d14f020b5ed4840bc3e814bb045b14d351a
root <lautarorodriguezcoitinho@gmail.com>
2023-02-22 12:07:32 -0800

    task0001

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - print upper
+ *
+ * Return: 1 if Upper
+ *
+ * @c: character v
+ */
+
+int _isdigit(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+
+}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,2 @@
-#include <stdio.h>
-#include <unistd.h>
-
 int _isupper(int c);
+int _isdigit(int c);
