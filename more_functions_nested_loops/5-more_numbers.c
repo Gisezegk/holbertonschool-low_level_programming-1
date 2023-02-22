1d580dc344e5d50215b57a7d3a496ff6965d974d
root <lautarorodriguezcoitinho@gmail.com>
2023-02-22 14:16:03 -0800

    taskcomplete

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* more_numbers - prints the alphabet.
+*
+*/
+
+void more_numbers(void)
+{
+	int f;
+	int xd;
+
+	for (f = 0; f <= 9; f++)
+	{
+		for (xd = 0; xd <= 14; xd++)
+		{
+			if (xd >= 10)
+			{
+				_putchar((xd / 10) + '0');
+				_putchar((xd % 10) + '0');
+			}
+		}
+	}
+	_putchar('\n');
+}
