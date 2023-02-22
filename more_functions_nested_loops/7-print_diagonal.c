8f5d8bc5b2adb2661e6a6b6c417d714df500ddc3
root <lautarorodriguezcoitinho@gmail.com>
2023-02-22 15:17:34 -0800

    asd

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,12 +8,15 @@
 void print_diagonal(int n)
 {
 	char c = 0;
+	char a = 0;
 
 	for (c = 0; c < n; c++)
-	{	
-		_putchar(' ');
-		_putchar(92);
-		_putchar('\n');
+	{
+		for (a = 0; a < n; a++)	 {
+			_putchar(' ');
+		}
+	_putchar(92);
+	_putchar('\n');
 	}
 	_putchar('\n');
 }
