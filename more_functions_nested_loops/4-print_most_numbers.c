b0efc6cac6fb4f664a8fbac685f9da63c60f81e8
root <lautarorodriguezcoitinho@gmail.com>
2023-02-22 13:31:10 -0800

    task

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_most_numbers - prints the alphabet.
+*
+*/
+
+void print_most_numbers(void)
+{
+	int abc;
+
+
+		for (abc = '0'; abc <= '9'; abc++)
+			if (abc !=  2 && abc != 4)
+			{
+				_putchar(abc);
+			}
+
+			else
+			{
+			}
+	_putchar('\n');
+}
+
