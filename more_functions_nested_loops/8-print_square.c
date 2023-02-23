bab0637c0c6b6ec07a9cec6f24f3c32f3138ddbe
root <lautarorodriguezcoitinho@gmail.com>
2023-02-23 04:29:50 -0800

    fixbetty

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
-* print_diagonal - prints the alphabet.
-*
-* @n: variable
+* print_square - prints the alphabet.
+*@size: square size
 */
 
 void print_square(int size)
 {
-        int c, f = 0;
+	int c, f = 0;
 
-        if (size > 0)
-        {
-        for (c = 0; c < size; c++)
-        {
-                for (f = 0; f < size; f++)
-                {
-                	_putchar(35);
-                }
-                _putchar('\n');
-        }
-        }
-        else
-        {
-        _putchar('\n');
-        }
+	if (size > 0)
+	{
+	for (c = 0; c < size; c++)
+	{
+		for (f = 0; f < sizie; f++)
+		{
+		_putchar(35);
+		}
+		_putchar('\n');
+	}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
