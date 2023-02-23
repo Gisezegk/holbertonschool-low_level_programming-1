3219a5238035962126f1bb4871e0d891f60d516e
root <lautarorodriguezcoitinho@gmail.com>
2023-02-23 07:58:14 -0800

    pleaseok

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_triangle - patric
+* Return: Always 0
+*/
+
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			if(b < size-a-1)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(35);
+			}
+		}
+		_putchar('\n');
+	}
+}
