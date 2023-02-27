3572a4ee3ead5d32b8b58bbe30bb10b35608e54e
root <lautarorodriguezcoitinho@gmail.com>
2023-02-27 09:15:40 -0800

    fixedbetty

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
-* swap_int - set
+* _strlen - set
 * @*s: var s
 * @s: var cjari
+* Return: always the size
 */
 
 int _strlen(char *s)
 {
-	int size;
-	size = 0;
-	
+	int size = 0;
+
 	while (*s != '\0')
 	{
 		size++;
