3cb1d0a5c44e174d7392074627ba14902d22dd04
root <lautarorodriguezcoitinho@gmail.com>
2023-02-28 13:10:56 -0800

    ready

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* puts2 - set
+* @a: var cjari
+* @n: var
+* Return: always the size
+*/
+
+void print_array(int *a, int n)
+{
+	int c = 0;
+
+	while (c < n)
+	{
+		printf("%d", a[c]);
+		c++;
+		if (c == n)
+		{
+			printf("%d", a[c]);
+		}
+	}
+}
