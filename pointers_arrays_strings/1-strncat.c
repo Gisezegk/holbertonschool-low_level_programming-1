bcec4fa870e2d5fd0225d9fbe1bc72813bc1aa0d
root <lautarorodriguezcoitinho@gmail.com>
2023-03-02 07:30:43 -0800

    franco

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -18,20 +18,20 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int fin = 0, cont = 0;
 
-	while (dest[fin] != '\0')
+	while (dest[fin])
 	{
 		fin++;
 	}
 	while (src[cont] < n)
 	{
-		dest[cont] = src[cont];
+		dest[fin] = src[cont];
 		cont++;
+	
+	if (src[cont] == '\0')
+	{
+		break;
+	}
 		fin++;
 	}
-	if (src[cont] == n)
-	{
-		src[cont] = '\0';
-	}
-
 	return (dest);
 }
