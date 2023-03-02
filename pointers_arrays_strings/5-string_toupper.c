d5687b54bc74adb2837f99f84e8d753b6f71fd97
root <lautarorodriguezcoitinho@gmail.com>
2023-03-02 13:46:46 -0800

    ready?

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * @c: count
+ * @u: string
+ *
+ * Return:
+ */
+
+
+char *string_toupper(char *u)
+{
+	int c;
+
+	while (u[c] != '\0')
+	{
+		if (u[c] >= 'a' && u[c] <= 'z')
+		{
+			u[c] = u[c] - 32;
+		}
+		c++;
+	}
+	return (u);
+}
