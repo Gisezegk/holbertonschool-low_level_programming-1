0fdbbbe53f18bf4e55e6130bfe4fa15e4020500e
root <lautarorodriguezcoitinho@gmail.com>
2023-03-02 08:17:36 -0800

    fixedall

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - s
+ *
+ * Return: always dest
+ *
+ * @n: limit
+ *
+ * @dest: given var
+ *
+ * @src: given var
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int fin = 0, pos = 0;
+
+	while (src[fin] != '\0')
+	{
+		fin++;
+	}
+	for (pos = 0; pos <= fin; pos++)
+	{
+		dest[pos] = src[pos];
+		if (pos == n)
+		{
+			break;
+		}
+		}
+	return (dest);
+}
