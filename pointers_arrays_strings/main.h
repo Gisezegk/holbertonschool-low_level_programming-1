97856d473073d110e86160eaf98d6248cbc88046
root <lautarorodriguezcoitinho@gmail.com>
2023-03-03 12:40:18 -0800

    l

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -28,3 +28,4 @@ unsigned int _strspn(char *s, char *accept);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
+void print_diagsums(int *a, int size);
