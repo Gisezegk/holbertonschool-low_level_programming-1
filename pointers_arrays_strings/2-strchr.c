9ad829d1161e10c331c5525a6e074ce8b0c77683
root <lautarorodriguezcoitinho@gmail.com>
2023-03-03 09:22:59 -0800

    test

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strchr - epico
+ * @c: var
+ * @c: tac
+ * Return: always var
+ *
+ */
+
+
+char *_strchr(char *s, char c)
+{
+	int cont = 0;
+
+	while (cont[s])
+	{
+		cont++;
+	
+	if (cont[s] == c)
+	{
+		return(s);
+	}
+	else
+	{
+	return ('\0');
+	}
+	
+	}
+	return(0);
+}
