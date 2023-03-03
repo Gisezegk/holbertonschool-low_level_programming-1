5fbb66affde011b491616cd52ffdfc17a4dcbe4b
root <lautarorodriguezcoitinho@gmail.com>
2023-03-03 12:55:20 -0800

    asd

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
-
-/**
- * _strpbrk - func
- *
- * @s: given
- * @accept: other given
- *
- * Return: null
- */
-
-
-char *_strpbrk(char *s, char *accept)
+char
+*_strpbrk(char *s, char *accept)
 {
 	int cont = 0, pos = 0;
 
 	while (s[cont] != '\0')
 	{
-		pos++;
 		while (accept[pos] != '\0')
 		{
 			if (s[cont] == accept[pos])
