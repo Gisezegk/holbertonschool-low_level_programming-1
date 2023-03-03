811e9987ec001098d19b119dcc12d780212e4aeb
root <lautarorodriguezcoitinho@gmail.com>
2023-03-03 11:11:41 -0800

    finish

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *
+ * _strspn - given
+ *
+ * @s: char
+ * @accept: given char
+ *
+ * Return: the cont
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int cont = 0, pos = 0, character = 0;
+
+	while (s[cont] != '\0' && accept[pos] != '\0')
+	{
+		{
+		if (accept[pos] == s[cont])
+		{
+			character++;
+		}
+		pos++;
+		}
+		character++;
+		}
+	return (character);
+}
