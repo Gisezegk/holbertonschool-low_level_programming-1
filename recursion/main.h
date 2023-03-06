e079e1b7fd3b1f8a9ac0ba0b89de8d85af08c0d2
root <lautarorodriguezcoitinho@gmail.com>
2023-03-06 10:20:00 -0800

    ready

--- recursion/main.h
+++ recursion/main.h
@@ -34,3 +34,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
