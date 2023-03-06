06b703686d085b260ac0d34ac44ee288057ff726
root <lautarorodriguezcoitinho@gmail.com>
2023-03-06 10:14:23 -0800

    main

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - given
+ *
+ * @s: chaian
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
+}
--- recursion/main.h
+++ recursion/main.h
@@ -33,3 +33,4 @@ char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
