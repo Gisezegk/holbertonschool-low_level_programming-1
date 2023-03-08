3203c844a898eb211c1415191186c527f0ed793d
root <lautarorodriguezcoitinho@gmail.com>
2023-03-08 08:27:43 -0800

    tamos

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+	int n1, n2, m;
+
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+
+	m = n1 * n2;
+	printf("%d\n", m);
+	return (0);
+}
