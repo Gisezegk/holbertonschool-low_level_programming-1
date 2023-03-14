044241f7fa2cc99042c32c97df451871924e851f
root <lautarorodriguezcoitinho@gmail.com>
2023-03-14 10:43:42 -0700

    task

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char
+*create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *a = 0;
+
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(size * sizeof(char));
+	if (a ==NULL)
+	{
+		return(NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
+}
