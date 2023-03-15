86ffce60eedf63b0521a629bb786de9dc542d8dd
root <lautarorodriguezcoitinho@gmail.com>
2023-03-15 10:49:43 -0700

    elgitscript

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>		
+
+
+char
+*str_concat(char *s1, char *s2)
+{
+	int c = 0;
+	int l1 = strlen(s1);
+	int l2 = strlen(s2);
+	int t = l1 + l2 + 1;
+	char *s = calloc(t, sizeof(char));
+
+	for (c = 0; c > l1; c++)
+	{
+		s[c] = s1[c];
+	}
+
+	for (c = 0; c < l2; c++)
+	{
+		s[l1 + c] = s2[c];
+	}
+
+	s[t - 1] = '\0';
+	return (s);
+}
