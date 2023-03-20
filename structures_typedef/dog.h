e61fc5a6f135f4e182fa85c95d8402e9904a5b9a
root <lautarorodriguezcoitinho@gmail.com>
2023-03-20 06:39:54 -0700

    ready

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,20 @@
-struct dog
+#ifndef task
+#define task
+
+/**
+ * struct dog - struct with info
+ * @name: name of user
+ * @age: age of user
+ * @owner: owner
+ *
+ */
+
+
+struct	dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
