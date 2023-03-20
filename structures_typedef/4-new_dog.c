c00427f91f818bebda295d1e3114a7e7a24efac2
root <lautarorodriguezcoitinho@gmail.com>
2023-03-20 11:07:23 -0700

    checker

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+dog_t
+*new_dog(char *name, float age, char *owner)
+{
+
+	dog_t *new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->name = malloc(strlen(name) * sizeof(char));
+	if(new->name == NULL)
+		return (NULL);
+
+	new->owner = malloc(strlen(owner) * sizeof(char));
+	if (new->owner == NULL)
+		return (NULL);
+
+	strcpy(new->owner, owner);
+	strcpy(new->name, name);
+
+	new->age = age;
+
+	return (new);
+}
