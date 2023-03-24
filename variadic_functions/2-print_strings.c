de09c522660931c7ba5efc37bed6eb53b78eb556
root <lautarorodriguezcoitinho@gmail.com>
2023-03-24 11:14:28 -0700

    practica

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -4,6 +4,7 @@ void
 print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *z = NULL;
 
 	va_list list;
 
@@ -11,7 +12,13 @@ print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(list, char *));
+		z = va_arg(list, char *);
+		
+		if (z == NULL)
+			printf("(nil)");
+		else
+			printf("%s", z);
+
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
