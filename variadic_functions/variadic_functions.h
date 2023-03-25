06387ebca91fe16682000de6230932c9903acd4d
Lautaro Rodriguez <81494428+LautareteX@users.noreply.github.com>
2023-03-25 16:35:34 -0300

    Update variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,6 +8,11 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_c(char a);
+void print_i(int a);
+void print_f(float a);
+void print_s(char *a);
 
 
 
